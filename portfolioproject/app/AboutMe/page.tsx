718db5bd3fee27d945fb7bf3fb64f6edf6f1106b
lucaferraresso <lucaferraresso96@gmail.com>
2024-10-02 18:16:47 +0200

    add navbar/footer/login/contacts/aboutme components

---
+++ portfolioproject/app/AboutMe/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AboutMe = () => {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <p>This is a simple about me page</p>
+    </div>
+  );
+};
+export default AboutMe;
---
+++ portfolioproject/app/Contacts/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Contacts = () => {
+  return (
+    <>
+      <div>
+        <h1>Contacts</h1>
+        <p>Phone: 000000000000</p>
+        <p>Email: info@example.com</p>
+      </div>
+    </>
+  );
+};
+
+export default Contacts;
