fd3ef464addcda0d667d61c48de348d99f336bf3
lucaferraresso <lucaferraresso96@gmail.com>
2024-10-03 11:45:53 +0200

    fix position (temporary)

--- portfolioproject/app/page.tsx
+++ portfolioproject/app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
 
   return (
     <>
-      <div className="mt-[400px] flex flex-col justify-center items-center border-2 h-[400px] w-[400px]">
+      <div className="mt-[400px] flex flex-col justify-center items-center text-center p-4 border-2">
         {/* Hero Section */}
         <motion.div
           className="border-2 p-4 flex justify-center items-center text-center "
--- portfolioproject/src/components/HeroSection.tsx
+++ portfolioproject/src/components/HeroSection.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import Image from "next/image";
 
 const HeroSection = () => {
   return (
     <div className="hero-section">
+      <Image src="/images/myFoto.jpg" alt="myFoto" width={200} height={200} />
       <h1>Hero Section</h1>
+      <p>Some text about me.</p>
+      <button>linkedin personale</button>
+      <button>github personale</button>
     </div>
   );
 };
