532a31447f17cb6ef941dea6ac5066767aa5de1d
lucaferraresso <lucaferraresso96@gmail.com>
2024-10-03 15:41:02 +0200

    design 1

--- portfolioproject/src/components/Footer.tsx
+++ portfolioproject/src/components/Footer.tsx
@@ -3,23 +3,24 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="w-full bg-[#1E1E1E] px-4 py-4 fixed bottom-0 left-0">
-      <div className="container mx-auto flex justify-between items-center">
-        <Link href="/">
-          <button className="text-white">
-            &copy; 2024 My Website. All rights reserved
-          </button>
-        </Link>
-        <div className="flex items-center gap-4">
-          <Link href="/Contacts">
-            <button className="text-white">Contacts</button>
-          </Link>
-          <Link href="/AboutMe">
-            <button className="text-white">AboutMe</button>
-          </Link>
-        </div>
-      </div>
+    <footer className="w-full bg-gray-200 p-4  fixed bottom-0 left-0 flex-col">
+      <Link href="/">
+        <button className="text-sm hover:text-gray-600 hover-underline-animation">
+          &copy; 2024 MyPortfolio. All rights reserved
+        </button>
+      </Link>
+      <Link href="/Contacts">
+        <button className="text-sm hover:text-gray-600 hover-underline-animation">
+          Contacts
+        </button>
+      </Link>
+      <Link href="/AboutMe">
+        <button className="text-sm hover:text-gray-600 hover-underline-animation">
+          AboutMe
+        </button>
+      </Link>
     </footer>
   );
 };
+
 export default Footer;
--- portfolioproject/src/components/Navbar.tsx
+++ portfolioproject/src/components/Navbar.tsx
@@ -1,25 +1,27 @@
 import Link from "next/link";
 import React from "react";
 
-const Navbar = () => {
+const Footer = () => {
   return (
-    <nav className="w-full fixed top-0 left-0 z-50 bg-[#1E1E1E] px-4 py-4 shadow-lg">
-      <div className="container mx-auto flex justify-between items-center">
-        <Link href="/">
-          <button className="text-white text-lg font-semibold">
-            Luca Ferraresso
-          </button>
-        </Link>
-        <div className="flex items-center gap-4">
-          <Link href="/Login">
-            <button className="text-white">Login</button>
-          </Link>
-          <Link href="/Projects">
-            <button className="text-white">Projects</button>
-          </Link>
-        </div>
-      </div>
-    </nav>
+    <footer className="w-full bg-gray-200 p-8 flex fixed bottom-0 left-0 flex-col">
+      <Link href="/">
+        <button className="text-sm hover:text-gray-600 hover-underline-animation">
+          Luca Ferraresso
+        </button>
+      </Link>
+
+      <Link href="/Login">
+        <button className="text-sm hover:text-gray-600 hover-underline-animation">
+          Login
+        </button>
+      </Link>
+      <Link href="/Projects">
+        <button className="text-sm hover:text-gray-600 hover-underline-animation">
+          Projects
+        </button>
+      </Link>
+    </footer>
   );
 };
-export default Navbar;
+
+export default Footer;
