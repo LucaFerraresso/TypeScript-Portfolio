138f117e74defdbfddb4b2d7f9a16141dcc64016
lucaferraresso <lucaferraresso96@gmail.com>
2024-10-10 20:02:01 +0200

    remove motiondiv on herosection

--- portfolioproject/src/components/HeroSection.tsx
+++ portfolioproject/src/components/HeroSection.tsx
@@ -6,72 +6,66 @@ import { fadeInVariants } from "@/animation/animation";
 
 const HeroSection: React.FC = () => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2">
-      <div>
-        <motion.div
-          variants={fadeInVariants}
-          initial="hidden"
-          animate="visible"
-          className="w-full flex  flex-col md:flex-row md:justify-around "
-        >
-          <Image
-            src="/images/myFoto.jpg"
-            alt="Luca Ferraresso"
-            height={250}
-            width={250}
-            className="rounded-3xl mt-4 mb-4"
-          />
-        </motion.div>
-      </div>
-
-      {/* Contenuto Testuale */}
-      <div className="w-full flex flex-col justify-center items-center p-8">
-        <motion.h2 variants={fadeInVariants} initial="hidden" animate="visible">
-          Luca Ferraresso
-        </motion.h2>
-        <motion.h1
-          initial={{ opacity: 0, y: 20 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 1, delay: 0.4 }}
-        >
-          Web Developer
-        </motion.h1>
-        <p>
-          Hi, I'm Luca Ferraresso. I am a passionete front-end developer based
-          near Venice. My expertise is focused on React and JavaScript, with the
-          goal always being to create responsive and engaging web experiences. I
-          love tacking complex challenges to transform them into intuitive and
-          user-friendly solutions
-        </p>
-
-        {/* Icone per LinkedIn e GitHub con animazioni */}
+    <motion.div variants={fadeInVariants} initial="hidden" animate="visible">
+      <div className="grid grid-cols-1 md:grid-cols-2">
         <div>
-          {" "}
           <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ duration: 1, delay: 0.8 }}
-            className="w-full flex flex-row"
+            variants={fadeInVariants}
+            initial="hidden"
+            animate="visible"
+            className="w-full flex  flex-col md:flex-row md:justify-around "
           >
-            <a
-              href="https://www.linkedin.com/in/tuo-linkedin"
-              target="_blank"
-              rel="noopener noreferrer"
-              aria-label="LinkedIn"
-            >
-              <FaLinkedin />
-            </a>
-            <a
-              href="https://github.com/tuo-github"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FaGithub />
-            </a>
+            <Image
+              src="/images/myFoto.jpg"
+              alt="Luca Ferraresso"
+              height={250}
+              width={250}
+              className="rounded-3xl mt-4 mb-4"
+            />
           </motion.div>
         </div>
+
+        {/* Contenuto Testuale */}
+        <div className="w-full flex flex-col justify-center items-center p-8 ">
+          <h2>Luca Ferraresso</h2>
+          <h1>Web Developer</h1>
+          <p>
+            Hi, I'm Luca Ferraresso. I am a passionete front-end developer based
+            near Venice. My expertise is focused on React and JavaScript, with
+            the goal always being to create responsive and engaging web
+            experiences. I love tacking complex challenges to transform them
+            into intuitive and user-friendly solutions
+          </p>
+
+          {/* Icone per LinkedIn e GitHub con animazioni */}
+          <div>
+            {" "}
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 1, delay: 0.8 }}
+              className="w-full flex flex-row"
+            >
+              <a
+                href="https://www.linkedin.com/in/tuo-linkedin"
+                target="_blank"
+                rel="noopener noreferrer"
+                aria-label="LinkedIn"
+              >
+                <FaLinkedin color="blue" />
+              </a>
+              <a
+                href="https://github.com/tuo-github"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaGithub color="black" />
+              </a>
+            </motion.div>
+          </div>
+        </div>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
